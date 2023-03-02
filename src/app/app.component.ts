f4ba2061d3f7e82dfa1b4a5802d6c97f68451f9e
amanyadav16 <amanyadav16011998@gmail.com>
2023-03-02 14:20:17 +0530

    bfvbvbf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn-with-fun';
+
+  myForm = new FormGroup({
+    age: new FormControl('',[Validators.required]),
+  })
+ 
+ 
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
 }
